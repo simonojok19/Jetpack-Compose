8679beba62e621814d98ff0eb662a475cb1c1611
simonojok19 <simonojok19@gmail.com>
2020-09-13 14:18:15 +0300

    started styling

--- app/src/main/java/com/example/jetpackexample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackexample/MainActivity.kt
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.graphics.Color
+import androidx.ui.text.TextStyle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello World")
+            Text(
+                    "Hello World",
+                    style = TextStyle(color = Color.Green)
+            )
         }
     }
 }
